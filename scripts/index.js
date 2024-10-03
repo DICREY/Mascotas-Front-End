383fe1b632dc93b2ed6a709fbddb43c38a8b407e
thedicrey <dicreyraptor@gmail.com>
2024-10-03 11:10:49 -0500

    V1.1.5

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded",() => {
-    
+    const btnMenu = document.querySelector(".btn-menu")
+    const menuDeploy = document.querySelector(".menu-deploy")
+
+    btnMenu.addEventListener("click",() => {
+        btnMenu.classList.toggle("rotate")
+
+        if (btnMenu.className == "bx bx-x btn-menu"){
+            btnMenu.className = "bx bx-menu btn-menu"
+            menuDeploy.classList.toggle("inactive")
+        } else {
+            btnMenu.className = "bx bx-x btn-menu"
+            menuDeploy.classList.toggle("inactive")
+        }
+    })
 })
