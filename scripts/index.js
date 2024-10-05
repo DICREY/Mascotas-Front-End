7e446633cf00f75abcc59fa7b7f5d3a7687864de
Dicrey <dicreyraptor@gmail.com>
2024-10-05 13:11:55 -0500

    V1.1.7

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded",() => {
     const btnMenu = document.querySelector(".btn-menu")
     const menuDeploy = document.querySelector(".menu-deploy")
+    const btnFloat = document.querySelector(".btn-float")
+    const btnFloatIcon = document.querySelector(".icon-btn-float")
+    const btnFloatWhatsapp = document.querySelector("#icon-whatsapp")
+    const btnFloatPhone = document.querySelector("#icon-phone")
+    const btnFloatGmail = document.querySelector("#icon-gmail")
+
+    btnFloat.addEventListener("click",floatBtn)
 
     btnMenu.addEventListener("click",() => {
         if (btnMenu.className === "bx bx-x btn-menu"){
@@ -11,4 +18,14 @@ document.addEventListener("DOMContentLoaded",() => {
             menuDeploy.classList.remove("inactive")
         }
     })
+
+    function floatBtn(){
+        const name = "bx bxs-chat icon-btn-float"
+        let nameClass = name == btnFloatIcon.className ? "bx bx-x icon-btn-float" : "bx bxs-chat icon-btn-float"
+        btnFloatIcon.className = nameClass
+        btnFloatWhatsapp.classList.toggle("icon-whatsapp")
+        btnFloatPhone.classList.toggle("icon-phone")
+        btnFloatGmail.classList.toggle("icon-gmail")
+    }
+
 })
