676b4f71f867f16941e6cfdf925fc8d47c1ce031
Dicrey <dicreyraptor@gmail.com>
2024-10-28 21:05:51 -0500

    V1.1.28

--- scripts/login.js
+++ scripts/login.js
@@ -1,17 +1,62 @@
 document.addEventListener("DOMContentLoaded",() => {
-    const iconPassword = document.querySelector(".i-password")
+    // containers 
+    const formLogin = document.getElementById("form-login")
+    const formDataPassword = document.getElementById("form-data-password")
+
+    // dynamic labels
+    const email = document.getElementById("email-forgot")
+    const name = document.getElementById("name-forgot")
+    const pastPassword = document.getElementById("password-forgot")
+
+    // btns 
+    const iconPassword = document.querySelectorAll(".i-password")
     const inputPassword = document.querySelector(".password")
+    const btnForgotPassword = document.getElementById("forgot-password")
+    const btnBacklog = document.getElementById("remember-password")
+    const btnSendPassword = document.getElementById("send-new-password")
 
-    iconPassword.addEventListener("click",showPassword)
+    // add events 
+    iconPassword.forEach(i => i.addEventListener("click",showPassword))
+    btnBacklog.addEventListener("click",logDatas)
+    btnForgotPassword.addEventListener("click",logDatas)
+    btnSendPassword.addEventListener("click",sendPassword)
 
+    // functions 
     function showPassword() {
+        const locked = "bx bx-lock icon i-password"
         const unlocked = "bx bx-lock-open icon i-password"
         if (iconPassword.className === unlocked) {
-            iconPassword.className = "bx bx-lock icon i-password"
+            iconPassword.className = locked
             inputPassword.type = "password"
         } else {
             iconPassword.className = unlocked
             inputPassword.type = "text"
         }
     }
+
+    function logDatas() {
+        formLogin.classList.toggle("inactive")
+        formDataPassword.classList.toggle("inactive")
+    }
+    
+    function sendPassword() {
+        const dataUser = [
+            {
+                email: "cristianarrietar4@gmail.com",
+                name: "cristian",
+                pastPassword: [
+                    "cristian123","cristian2025#","cristian2024"
+                ]
+            }
+        ]
+        const emailValue = email.value
+        const nameValue = name.value
+        const pastPasswordValue = pastPassword.value
+        dataUser.forEach(i => i.name === nameValue || i.email === emailValue? 
+            alert(`Review your email ${emailValue}`): alert("Datas not found"))
+        setTimeout(() => {
+            location.reload()
+        },3000)
+    }
+
 })
